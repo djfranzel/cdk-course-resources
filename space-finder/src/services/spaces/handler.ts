be76ca70a04f1409fed941ecef70878779875f55
David Franzel <djfranzel@pm.me>
2024-02-12 16:46:28 -0600

    scan almost working but permission error in aws

--- space-finder/src/services/spaces/handler.ts
+++ space-finder/src/services/spaces/handler.ts
@@ -1,18 +1,29 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { postSpaces } from "./PostSpaces";
+import { getSpaces } from "./GetSpaces";
+
+const ddbClient = new DynamoDBClient({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
     let message: string;
 
-    switch (event.httpMethod) {
-        case 'GET':
-            message = 'Hello from GET!'
-            break;
-        case 'POST':
-            message = 'Hello from POST!'
-            break;
-        default:
-            break;
+    try {
+        switch (event.httpMethod) {
+            case 'GET':
+                return await getSpaces(event, ddbClient);
+            case 'POST':
+                return postSpaces(event, ddbClient);
+            default:
+                break;
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error.message)
+        }
     }
 
 
