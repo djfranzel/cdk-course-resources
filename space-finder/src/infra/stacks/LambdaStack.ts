b158febb683440a33b163d2eed6c70ffadd17c97
David Franzel <djfranzel@pm.me>
2024-02-12 14:36:36 -0600

    working ls on s3 from lambda

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -3,19 +3,38 @@ import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+
+interface LambdaStackProps extends StackProps {
+    spacesTable: ITable
+}
 
 export class LambdaStack extends Stack {
 
     public readonly helloLambdaIntegration: LambdaIntegration;
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
+    constructor(scope: Construct, id: string, props?: LambdaStackProps) {
         super(scope, id, props);
 
-        const helloLambda = new LambdaFunction(this, 'HelloLambda', {
+        const helloLambda = new NodejsFunction(this, 'HelloLambda', {
             runtime: Runtime.NODEJS_20_X,
-            handler: 'hello.main',
-            code: Code.fromAsset(join(__dirname, '../../services'))
+            handler: 'handler',
+            entry: join(__dirname, '../../services/hello.ts'),
+            environment: {
+                TABLE_NAME: props.spacesTable.tableName
+            }
         });
+
+        helloLambda.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                's3:ListAllMyBuckets',
+                's3:ListBucket',
+            ],
+            resources: ['*'] // bad practice - be more specific
+        }))
 
         this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
     }
---
+++ space-finder/src/test/launcher.ts
@@ -0,0 +1,4 @@
+import { handler } from "../services/hello";
+
+
+handler({} as any, {} as any);
